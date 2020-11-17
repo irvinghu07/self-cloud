b3f1fe5057d353d5dac24ae1055cfb6783056c4d
irvinghu07 <rhu5@dons.usfca.edu>
2020-11-18 00:17:30 +0800

    refactoring using gin-gonic

---
+++ src/main/gin-server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func singleUploadHandler(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err == nil {
+		filename := file.Filename
+		cwd, err := os.Getwd()
+		dir := "/resources/"
+		if err == nil {
+			err := c.SaveUploadedFile(file, cwd+dir+filename)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, fmt.Sprintf("'%s' upload failed:%v", filename, err))
+			} else {
+				c.JSON(http.StatusOK, fmt.Sprintf("'%s' uploaded!", filename))
+			}
+		}
+	} else {
+		log.Fatal(err)
+	}
+}
+
+func multiUploadHandler(context *gin.Context) {
+
+}
+
+func main() {
+	routes := gin.Default()
+	routes.MaxMultipartMemory = 8 << 20
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8080"}
+	uploadGroup := routes.Group("/upload")
+	{
+		uploadGroup.POST("/single", singleUploadHandler)
+		uploadGroup.POST("/multi", multiUploadHandler)
+	}
+	err := routes.Run(":7777")
+	if err != nil {
+		log.Fatal("gin startup")
+	}
+}
